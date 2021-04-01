b64f5b4bffdab28a6a5eb9ecae6a3ae68c4b95a0
Saurabhsahab <63358661+Saurabhsahab@users.noreply.github.com>
2021-04-01 11:06:32 +0530

    W1P1

--- Week 4/The most frequent symbol.cpp
+++ Week 4/The most frequent symbol.cpp
@@ -92,11 +92,6 @@ int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(0);
 
-#ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-#endif
-
     //cout<<fixed<<setprecision(10)
 
     string s;
