fc7b81fe3fb45f676edc7f44a1c44114a46cc20b
Saurabhsahab <63358661+Saurabhsahab@users.noreply.github.com>
2021-04-01 11:02:29 +0530

    W4P1

---
+++ Week 4/The most frequent symbol.cpp
@@ -0,0 +1,163 @@
+/***
+⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡖⠁⠀⠀⠀⠀⠀⠀⠈⢲⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⣼⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣧⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⣸⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⣇⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⣿⣿⡇⠀⢀⣀⣤⣤⣤⣤⣀⡀⠀⢸⣿⣿⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⣔⢿⡿⠟⠛⠛⠻⢿⡿⣢⣿⣿⡟⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⣀⣤⣶⣾⣿⣿⣿⣷⣤⣀⡀⢀⣀⣤⣾⣿⣿⣿⣷⣶⣤⡀⠀⠀⠀⠀
+⠀⠀⢠⣾⣿⡿⠿⠿⠿⣿⣿⣿⣿⡿⠏⠻⢿⣿⣿⣿⣿⠿⠿⠿⢿⣿⣷⡀⠀⠀
+⠀⢠⡿⠋⠁⠀⠀⢸⣿⡇⠉⠻⣿⠇⠀⠀⠸⣿⡿⠋⢰⣿⡇⠀⠀⠈⠙⢿⡄⠀
+⠀⡿⠁⠀⠀⠀⠀⠘⣿⣷⡀⠀⠰⣿⣶⣶⣿⡎⠀⢀⣾⣿⠇⠀⠀⠀⠀⠈⢿⠀
+⠀⡇⠀⠀⠀⠀⠀⠀⠹⣿⣷⣄⠀⣿⣿⣿⣿⠀⣠⣾⣿⠏⠀⠀⠀⠀⠀⠀⢸⠀
+⠀⠁⠀⠀⠀⠀⠀⠀⠀⠈⠻⢿⢇⣿⣿⣿⣿⡸⣿⠟⠁⠀⠀⠀⠀⠀⠀⠀⠈⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣼⣿⣿⣿⣿⣧⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠐⢤⣀⣀⢀⣀⣠⣴⣿⣿⠿⠋⠙⠿⣿⣿⣦⣄⣀⠀⠀⣀⡠⠂⠀⠀⠀
+⠀⠀⠀⠀⠀⠈⠉⠛⠛⠛⠛⠉⠀⠀⠀⠀⠀⠈⠉⠛⠛⠛⠛⠋⠁⠀⠀
+
+   * Author : SUARABH UPADHAYAY
+   * E-mail : officialsaurabhupadhayay@gmail.com
+
+   PRACTICE LIKE YOU NEVER WIN, PLAY LIKE YOU NEVER LOOSE
+
+
+***/
+
+#include<bits/stdc++.h>
+//#include <boost/math/common_factor.hpp>
+//#include <boost/multiprecision/cpp_int.hpp>
+//using namespace boost::multiprecision;
+using namespace std;
+#define lll cpp_int
+#define ull unsigned long long
+#define ll long long
+#define ui unsigned int
+#define ldb long double
+//#define lcm(o,g) boost::math::lcm(o,g)
+#define mod 1000000007
+#define mod1 1000003
+#define mod2 998244353
+#define pi acos(-1)
+#define full LLONG_MAX
+#define V vector
+#define VL vector<ll>
+#define LL list<ll>
+#define DL deque<ll>
+#define PQL priority_queue<ll>
+#define SL set<ll>
+#define SC set<char>
+#define M map
+#define MLL map<ll,ll>
+#define SS stringstream
+#define mkp make_pair
+#define mkt make_tuple
+#define er equal_range
+#define lb lower_bound
+#define ub upper_bound
+#define bs binary_search
+#define np next_permutation
+#define ln length()
+#define em empty()
+#define DO greater<ll>()
+#define DM greater<ll>
+#define pb push_back
+#define in insert
+#define pob pop_back()
+#define bg begin()
+#define fr front()
+#define ed end()
+#define bk back()
+#define sz size()
+#define all(o) ((o).bg,(o).ed)
+#define alld(o) ((o).bg,(o).ed,DO)
+#define F first
+#define S second
+#define stf shrink_to_fit()
+#define ig cin.ignore(1,'\n');
+#define cg(g) getline(cin,(g))
+#define f(g,h,o) for(ll g=h;g<o;g++)
+#define f1(g,h,o) for(ll g=h;g>o;g--)
+#define f2(g) for(auto E : (g))
+#define fm(g) for(auto [X,Y] : (g))
+#define AI(g,o) VL g(o); f(i,0,o)cin>>g[i]
+#define AO(g,o) f(i,0,o)cout<<g[i]<<" "
+#define T ui t; cin>>t; while(t--)
+#define T1(g) ui g; cin>>g; while(g--)
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimize("unroll-loops")
+
+int main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    //cout<<fixed<<setprecision(10)
+
+    string s;
+    cin>>s;
+    ll a[s.ln][26]={(0,0)};
+
+    f(i,0,s.ln){
+    	ll k=(ll)(s[i]-'a');
+
+    	if(i==0){
+    		a[i][k]++;
+    	}
+    	else{
+    		f(j,0,26){
+    			if(j!=k){
+    				a[i][j]=a[i-1][j];
+    			}
+    			else{
+    				a[i][j]=a[i-1][j];
+    				a[i][j]++;
+    			}
+    		}
+    	}
+    }
+
+    T{
+    	ll l,r;
+    	cin>>l>>r;
+    	l-=2;
+    	r--;
+
+    	if(l==r){
+    		cout<<s[l]<<"\n";
+    	}
+    	else{
+
+    		if(l<0){
+
+    			ll p=-1,q=-full;
+              	f(i,0,26){
+              		if(a[r][i]>q){
+              			q=a[r][i];
+              			p=i;
+              		}
+              	}
+
+              	cout<<(char)(p+'a')<<"\n";
+    		}
+    		else{
+
+    			ll p=-1,q=-full;
+              	f(i,0,26){
+              		ll k=abs(a[r][i]-a[l][i]);
+              		if(k>q){
+              			q=k;
+              			p=i;
+              		}
+              	}
+
+              	cout<<(char)(p+'a')<<"\n";
+    		}
+    	}
+    }
+}
