bea669474c2ee372fa22ab3f08f8239ea4b7eec1
Saurabhsahab <63358661+Saurabhsahab@users.noreply.github.com>
2021-04-02 12:11:56 +0530

    W4P2
    !

---
+++ Week 4/Maximal distance.cpp
@@ -0,0 +1,175 @@
+/***
+⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡖⠁⠀⠀⠀⠀⠀⠀⠈⢲⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⣼⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣧⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⣸⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⣇⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⣿⣿⡇⠀⢀⣀⣤⣤⣤⣤⣀⡀⠀⢸⣿⣿⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⣔⢿⡿⠟⠛⠛⠻⢿⡿⣢⣿⣿⡟⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⣀⣤⣶⣾⣿⣿⣿⣷⣤⣀⡀⢀⣀⣤⣾⣿⣿⣿⣷⣶⣤⡀⠀⠀⠀⠀
+⠀⠀⢠⣾⣿⡿⠿⠿⠿⣿⣿⣿⣿⡿⠏⠻⢿⣿⣿⣿⣿⠿⠿⠿⢿⣿⣷⡀⠀⠀
+⠀⢠⡿⠋⠁⠀⠀⢸⣿⡇⠉⠻⣿⠇⠀⠀⠸⣿⡿⠋⢰⣿⡇⠀⠀⠈⠙⢿⡄⠀
+⠀⡿⠁⠀⠀⠀⠀⠘⣿⣷⡀⠀⠰⣿⣶⣶⣿⡎⠀⢀⣾⣿⠇⠀⠀⠀⠀⠈⢿⠀
+⠀⡇⠀⠀⠀⠀⠀⠀⠹⣿⣷⣄⠀⣿⣿⣿⣿⠀⣠⣾⣿⠏⠀⠀⠀⠀⠀⠀⢸⠀
+⠀⠁⠀⠀⠀⠀⠀⠀⠀⠈⠻⢿⢇⣿⣿⣿⣿⡸⣿⠟⠁⠀⠀⠀⠀⠀⠀⠀⠈⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣼⣿⣿⣿⣿⣧⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠐⢤⣀⣀⢀⣀⣠⣴⣿⣿⠿⠋⠙⠿⣿⣿⣦⣄⣀⠀⠀⣀⡠⠂⠀⠀⠀
+⠀⠀⠀⠀⠀⠈⠉⠛⠛⠛⠛⠉⠀⠀⠀⠀⠀⠈⠉⠛⠛⠛⠛⠋⠁⠀⠀
+
+   * Author : SUARABH UPADHAYAY
+   * E-mail : officialsaurabhupadhayay@gmail.com
+
+   PRACTICE LIKE YOU NEVER WIN, PLAY LIKE YOU NEVER LOOSE
+
+
+***/
+
+#include<bits/stdc++.h>
+//#include <boost/math/common_factor.hpp>
+//#include <boost/multiprecision/cpp_int.hpp>
+//using namespace boost::multiprecision;
+using namespace std;
+#define lll cpp_int
+#define ull unsigned long long
+#define ll long long
+#define ui unsigned int
+#define ldb long double
+//#define lcm(o,g) boost::math::lcm(o,g)
+#define mod 1000000007
+#define mod1 1000003
+#define mod2 998244353
+#define pi acos(-1)
+#define full LLONG_MAX
+#define V vector
+#define VL vector<ll>
+#define LL list<ll>
+#define DL deque<ll>
+#define PQL priority_queue<ll>
+#define SL set<ll>
+#define SC set<char>
+#define M map
+#define MLL map<ll,ll>
+#define SS stringstream
+#define mkp make_pair
+#define mkt make_tuple
+#define er equal_range
+#define lb lower_bound
+#define ub upper_bound
+#define bs binary_search
+#define np next_permutation
+#define ln length()
+#define em empty()
+#define DO greater<ll>()
+#define DM greater<ll>
+#define pb push_back
+#define in insert
+#define pob pop_back()
+#define bg begin()
+#define fr front()
+#define ed end()
+#define bk back()
+#define sz size()
+#define all(o) ((o).bg,(o).ed)
+#define alld(o) ((o).bg,(o).ed,DO)
+#define F first
+#define S second
+#define stf shrink_to_fit()
+#define ig cin.ignore(1,'\n');
+#define cg(g) getline(cin,(g))
+#define f(g,h,o) for(ll g=h;g<o;g++)
+#define f1(g,h,o) for(ll g=h;g>o;g--)
+#define f2(g) for(auto E : (g))
+#define fm(g) for(auto [X,Y] : (g))
+#define AI(g,o) VL g(o); f(i,0,o)cin>>g[i]
+#define AO(g,o) f(i,0,o)cout<<g[i]<<" "
+#define T ui t; cin>>t; while(t--)
+#define T1(g) ui g; cin>>g; while(g--)
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimize("unroll-loops")
+
+VL a(1000001);
+V<pair<ll,ll>> b(1000001);
+
+void bt(ll si,ll ss,ll se){
+	if(ss>se)
+		return;
+
+	if(ss==se){
+		b[si].F=se;
+		b[si].S=se;
+		return;
+	}
+
+	ll m=(ss+se)/2;
+
+	bt(2*si,ss,m);
+	bt(2*si+1,m+1,se);
+
+	if(a[b[2*si].F]>=a[b[2*si+1].F])
+		b[si].F=b[2*si].F;
+	else
+		b[si].F=b[2*si+1].F;
+
+	if(a[b[2*si].S]<=a[b[2*si+1].S])
+		b[si].S=b[2*si].S;
+	else
+		b[si].S=b[2*si+1].S;
+
+	return;
+}
+
+pair<ll,ll> qu(ll si,ll ss,ll se,ll l,ll r){
+	if(ss>se)
+		return mkp(0,1000000);
+	if(ss>r||se<l)
+		return mkp(0,1000000);
+
+	if(ss>=l&&se<=r)
+		return b[si];
+
+	ll m=(ss+se)/2;
+
+	pair<ll,ll> p=qu(2*si,ss,m,l,r);
+	pair<ll,ll> q=qu(2*si+1,m+1,se,l,r);
+
+    ll x,y;
+
+    if(a[p.F]>=a[q.F])
+		x=p.F;
+	else
+		x=q.F;
+
+	if(a[p.S]<=a[q.S])
+		y=p.S;
+	else
+		y=q.S;
+
+	return mkp(x,y);
+}
+
+
+
+int main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    //cout<<fixed<<setprecision(10)
+
+    ll n;
+    cin>>n;
+
+    f(i,1,n+1)
+    cin>>a[i];
+    a[0]=-full;
+    a[1000000]=full;
+
+    bt(1,1,n);
+
+    f(i,1,n+1){
+    	pair<ll,ll> c=qu(1,1,n,1,i);
+
+    	cout<<c.F<<" "<<c.S<<"\n";
+    }
+
+}
